304a98e171035e9167485b1c4153fe62d562fe43
hyprj <Krzysztofzur21@gmail.com>
2022-04-14 21:03:41 +0200

    improve navigation bar

--- client/src/Components/UI/NavBar.js
+++ client/src/Components/UI/NavBar.js
@@ -7,13 +7,18 @@ const NavBar = () => {
     <header className={classes.header}>
       <nav className={classes.nav}>
         <Link to="/" className="reset">
-          <p className={classes["nav__logo"]}>Time Efficient</p>
+          <p className={classes["nav__logo"]}>WorkTime</p>
         </Link>
         <ul className={classes["nav__list"]}>
           <ol className={classes["nav__elem"]}>
             <a href="" className={classes["nav__link"]}>
               About
             </a>
+          </ol>
+          <ol className={classes["nav__elem"]}>
+            <Link to="schedule" className={classes["nav__link"]}>
+              Schedule
+            </Link>
           </ol>
           <ol className={classes["nav__elem"]}>
             <Link to="employees/1" className={classes.btn}>
